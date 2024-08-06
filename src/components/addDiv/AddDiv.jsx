4c28659bdb4731b4f71d179c68122b7e7c1429f3
Charles Seaton <seatonct@gmail.com>
2024-08-06 14:37:21 -0500

    fixed delete block button

--- src/components/addDiv/AddDiv.jsx
+++ src/components/addDiv/AddDiv.jsx
@@ -9,25 +9,19 @@ export const AddDiv = () => {
   //   };
 
   const deleteDiv = (index) => {
-    setDivs(divs.filter((div) => div.id !== index));
+    setDivs(divs.filter((div) => div.id != index));
   };
 
   const addDiv = () => {
-    const divId = divs.length;
-    setDivs([
-      ...divs,
-      <div key={divId} id={divId} className="bg-gray-300 p-4 m-2 rounded">
-        <textarea></textarea>
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => {
-            deleteDiv(divId);
-          }}
-        >
-          Delete Block
-        </button>
-      </div>,
-    ]);
+    const newDiv = {
+      id: divs.length,
+      content: (
+        <div className="bg-gray-300 p-4 m-2 rounded">
+          <textarea></textarea>
+        </div>
+      ),
+    };
+    setDivs([...divs, newDiv]);
   };
 
   return (
@@ -38,7 +32,19 @@ export const AddDiv = () => {
       >
         Add Block
       </button>
-      <div className="w-full flex flex-col items-center">{divs}</div>
+      <div className="w-full flex flex-col items-center">
+        {divs.map((div) => (
+          <div key={div.id}>
+            {div.content}
+            <button
+              className="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+              onClick={() => deleteDiv(div.id)}
+            >
+              Delete Block
+            </button>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
